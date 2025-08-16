1a6d362c27b27f5037e55d41a9c86ffdaa4d2a65
Bhushanpatil <bhushanpatilbkp68@gmail.com>
2025-08-16 12:32:59 +0530

    updated config.yml and config.go :

--- internal/config/config.go
+++ internal/config/config.go
@@ -18,12 +18,27 @@ type Config struct {
 		Name     string
 		SSLMode  string
 	}
+	Auth struct {
+		JWTSecret string
+	}
 }
 
 func LoadConfig() *Config {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("./configs")
+
+	// env overrides: APP_SERVER_PORT etc.
+	viper.SetEnvPrefix("APP")
+	viper.AutomaticEnv()
+	_ = viper.BindEnv("server.port", "APP_SERVER_PORT")
+	_ = viper.BindEnv("database.host", "APP_DB_HOST")
+	_ = viper.BindEnv("database.port", "APP_DB_PORT")
+	_ = viper.BindEnv("database.user", "APP_DB_USER")
+	_ = viper.BindEnv("database.password", "APP_DB_PASSWORD")
+	_ = viper.BindEnv("database.name", "APP_DB_NAME")
+	_ = viper.BindEnv("database.sslmode", "APP_DB_SSLMODE")
+	_ = viper.BindEnv("auth.jwtsecret", "APP_JWT_SECRET")
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error loading config: %v", err)
@@ -33,6 +48,5 @@ func LoadConfig() *Config {
 	if err := viper.Unmarshal(&cfg); err != nil {
 		log.Fatalf("Unable to decode config: %v", err)
 	}
-
 	return &cfg
 }
