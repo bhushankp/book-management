fe59a185207076c1d8e196dd7303f48d8225b60e
Bhushanpatil <bhushanpatilbkp68@gmail.com>
2025-08-16 12:24:19 +0530

    add graceful shtdown and health endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"book-management/internal/config"
 	"book-management/internal/db"
-	"book-management/internal/models"
 	"book-management/internal/pkg/logger"
 	"book-management/internal/pkg/validator"
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
 
 	"go.uber.org/zap"
 )
@@ -17,13 +24,38 @@ func main() {
 	logger.InitLogger()
 	db.Connect(cfg)
 	validator.Init()
-	logger.Log.Info("config loaded",
-		zap.Int("port", cfg.Server.Port),
-	)
 
-	book := models.Book{Title: "", Author: "Unknown"}
-	if err := validator.ValidateStruct(book); err != nil {
-		fmt.Println("Validation failed:", err)
+	// Setup router
+	r := mux.NewRouter()
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Server.Port),
+		Handler: r,
 	}
+
+	// Graceful shutdown
+	go func() {
+		logger.Log.Info("🚀 Server running", zap.Int("port", cfg.Server.Port))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Log.Fatal("Server failed", zap.Error(err))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logger.Log.Info("Shutting down gracefully...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Log.Fatal("Server forced to shutdown", zap.Error(err))
+	}
+
+	logger.Log.Info("Server exited properly")
 
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.6
 
 require (
 	github.com/go-playground/validator/v10 v10.15.0
+	github.com/gorilla/mux v1.8.1
 	github.com/spf13/viper v1.20.1
 	go.uber.org/zap v1.27.0
 	gorm.io/driver/mysql v1.6.0
